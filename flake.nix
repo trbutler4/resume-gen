42881346d049a015883835268a2a3ac82d985715
trbutler4 <trbiv@proton.me>
2026-01-17 00:03:10 -0600

    adding flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Resume generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.bun ];
+          };
+        });
+    };
+}
